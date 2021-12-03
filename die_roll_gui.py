02e4c22d34e13de68e7f909a115deeedc8de33d8
allisondaviss <77467121+allisondaviss@users.noreply.github.com>
2021-12-03 17:09:51 -0500

    Update die_roll_gui.py

--- die_roll_gui.py
+++ die_roll_gui.py
@@ -1,33 +1,25 @@
 from tkinter import *
 import random
 import tkinter
+from PIL import Image, ImageTk, ImageSequence
+import time
 
 root = Tk()
 root.geometry("400x400")
 root.title("Allison's Dice Roll Simulator")
 
-dice = ['dieone.jpg', 'dietwo.jpg', 'diethree.jpg', 'diefour.jpg', 'diefive.jpg', 'diesix.jpg']
-image1 = PhotoImage(Image.open(random.choice(dice)))
-image2 = PhotoImage(Image.open(random.choice(dice)))
+dilist = ['dieone.jpg', 'dietwo.jpg', 'diethree.jpg', 'diefour.jpg', 'diefive.jpg', 'diesix.jpg']
+diopen = ImageTk.PhotoImage(Image.open('dieone.jpg'))
+label = Label(root)
+label.pack()
 
-label1 = tkinter.Label(root, image=image1)
-label2 = tkinter.Label(root, image=image2)
+def dice_roll():
+    path = random.choice(dilist)
+    imag = ImageTk.PhotoImage(Image.open(path))
+    label.place(x=150, y=300)
+    label.configure(image=imag)
+    label.image = imag
+    time.sleep(0.03)
 
-label1.image = image1
-label2.image = image2
-label1.pack(side=tkinter.LEFT)
-label2.pack(side=tkinter.RIGHT)
-
-def roll():
-    image1 = PhotoImage(Image.open(random.choice(dice)))
-    label1.configure(image=image1)
-    label1.image = image1
-    image2  = PhotoImage(Image.open(random.choice(dice)))
-    label2.configure(image=image2)
-    label2.image = image2
-
-b1 = Button(root,text="Roll the Dice!",foreground='blue',command=roll)
-b1.place(x=300,y=0)
-b1.pack()
-
+Button(root,text="Click to roll the die!", height=5, width=10, bg='green', command=dice_roll).place(x=400, y=300)
 root.mainloop()
