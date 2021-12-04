ae42b08b9da9f3737d7169187dec3e002d2808af
allisondaviss <77467121+allisondaviss@users.noreply.github.com>
2021-12-03 19:19:56 -0500

    (1) Large Code Change prior; changing bg color of label and window
    I tried to use images of all sides of a die and have the program select two randomly and display these photos to roll the dice. Instead I used unicode.  Also changed bg color of label for the dice and the window.

--- die_roll_gui.py
+++ die_roll_gui.py
@@ -5,6 +5,7 @@ import tkinter
 root=Tk()
 root.geometry("400x400")
 root.title("Allison's Dice Roll Simulator")
+root.configure(bg='gray77')
 
 label1 = Label(root,font=("Helvetica",260))
 
@@ -12,6 +13,7 @@ def dice_roll():
     dice = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
     label1.config(text=f'{random.choice(dice)}{random.choice(dice)}')
     label1.pack()
+    label1.configure(bg='gray77')
 
 button = Button(root, text="Click to Roll the Dice!", foreground='green',command=dice_roll)
 button.place(x=300,y=0)
