869d2edf6300395f8472263dbc26b85ed9f2efdd
allisondaviss <77467121+allisondaviss@users.noreply.github.com>
2021-12-13 21:33:50 -0500

    final cosmetic adjustments
    Changed opacity of window and size and aspects of text

--- die_roll_gui.py
+++ die_roll_gui.py
@@ -4,7 +4,7 @@ import random
 root=Tk()
 root.geometry("400x400")
 root.title("Allison's Dice Roll Simulator")
-root.configure(bg='gray77')
+root.configure(bg='LightBlue3')
 root.attributes('-alpha', 0.9)
 
 label1 = Label(root,font=("Helvetica",260))
@@ -13,13 +13,15 @@ def dice_roll():
     dice = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
     label1.config(text=f'{random.choice(dice)}{random.choice(dice)}')
     label1.pack()
-    label1.configure(bg='gray77')
+    label1.configure(bg='LightBlue3')
 
-button = Button(root, text="Click to Roll the Dice!", font = 'Helvetica 15 underline' , bg='light blue', foreground='black',command=dice_roll, height=3, width=15)
+button = Button(root, text="CLICK TO ROLL THE DICE!", font = 'Helvetica 15 underline bold', foreground='black', command=dice_roll, height=2, width=25)
 button.pack(side=TOP)
 
+def close():
+    root.destroy()
 
-button1 = Button(root, text="Exit", foreground = 'black', command=root.destroy, height=2, width=4)
+button1 = Button(root, text="EXIT", foreground = 'black', command=close, font = 'Helvetica 15 bold', height=2, width=5)
 button1.pack(side=BOTTOM)
 
 
